a5c0bf38e3caa8297955fb2279c14d09f07e5b25
SeguRuiz <ruizsegura96199@gmail.com>
2025-01-24 17:38:46 -0600

    Error de navbar arreglado

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,29 @@ import Navbar from "./components/Navbar";
 import { Routes } from "react-router";
 import { useLocation } from "react-router";
 
-import MotionPage from "./components/MotionPage";
+
 import { useEffect } from "react";
-import { useNavigate } from "react-router";
+
 import Dashboard from "./dashboard/Dashboard";
-import SignInCard from "./sign-in-side/components/SignInCard";
+
 import SignInSide from "./sign-in-side/SignInSide";
 
 const Pages = [];
 
 function App() {
   const location = useLocation();
-  const navigate = useNavigate();
+  
 
   useEffect(() => {
-    // location.pathname == "/" && navigate("/components/scroll");
+    
 
     window.scrollTo(0, 0);
   }, [location.pathname]);
 
   return (
     <>
-      {location.pathname != "/admin" && <Navbar Pages={Pages} />}
+      {location.pathname == "/" && <Navbar Pages={Pages} />}
       <Routes>
-       
         <Route path="/admin/:parte" element={<Dashboard />} />
         <Route path="/" element={<SignInSide />} />
       </Routes>
