ef54f3c75370f8255c24f9be291f8e53f23f4988
SeguRuiz <ruizsegura96199@gmail.com>
2025-01-23 23:46:54 -0600

    Definicion de constantes

---
+++ src/constants/animals.d.js
@@ -0,0 +1,15 @@
+export const ANIMALES_GANADO_ESPECIES = {
+  BOVINO: "BOVINO", //  (Vacas y Toros)
+  OVINOS: "OVINOS", //  (Ovejas)
+  PORCINOS: "PORCINOS", // (Cerdos)
+  CAPRINOS: "CAPRINOS", // (Cabras
+  EQUINOS: "EQUINOS", // (Caballos)
+  AVES_DE_CORRAL: "AVES_DE_CORRAL", // (Gallinas, Pollos, Pavos, Patos, Gansos)
+};
+
+export const ANIMALES_ESTADO = {
+  MUERTO: "MUERTO",
+  ENFERMO: "ENFERMO",
+  AGITADO: "AGITADO",
+  SANO: "SANO",
+};
