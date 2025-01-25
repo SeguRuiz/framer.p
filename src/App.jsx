d4f6248d34288997ad641b1ff47d63a6bb82e109
W-Alexander <alexanderaguilar726@gmail.com>
2025-01-25 03:15:28 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,16 @@ import { Route, useNavigate } from "react-router";
 import Navbar from "./components/Navbar";
 import { Routes } from "react-router";
 import { useLocation } from "react-router";
-
 import { useEffect } from "react";
-
 import Dashboard from "./dashboard/Dashboard";
-
 import SignInSide from "./sign-in-side/SignInSide";
 import { getCookie } from "./utils/cookies";
-
 import PCambio_estado from "./pages/PCambio_estado/PCambio_estado";
 
+
 const Pages = [];
+
+  
 
 function App() {
   const location = useLocation();
@@ -23,11 +22,14 @@ function App() {
     userInfo && navigate("/admin/home");
   }, [location.pathname]);
 
+  
+
   return (
     <>
       {!userInfo ? <Navbar Pages={Pages} /> : null}
       <Routes>
         {userInfo != "" && (
+
           <Route path="/admin/:seccion" element={<Dashboard />} />
         )}
         {!userInfo && <Route path="/" element={<SignInSide />} />}
