e88d032d28bad67a87c5a4d8ddb86580a9fdc97d
SeguRuiz <ruizsegura96199@gmail.com>
2025-01-26 15:15:03 -0600

    componet

--- src/dashboard/internals/components/Copyright.jsx
+++ src/dashboard/internals/components/Copyright.jsx
@@ -17,7 +17,7 @@ export default function Copyright(props) {
     >
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Sitemark
+        SmartCow
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
